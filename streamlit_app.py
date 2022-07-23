eed67c6c766b9b884dbb67a8ae70640d575289f5
Sivaldo Barros <104164899+sivaldobarros@users.noreply.github.com>
2022-07-23 14:02:42 -0300

    teste

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-@st.cache
+
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
@@ -18,7 +18,7 @@ def load_data(nrows):
 
 data_load_state = st.text('Loading data...')
 data = load_data(10000)
-data_load_state.text("Done! (using st.cache)")
+data_load_state.text('Loading data...done!')
 
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
