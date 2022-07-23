c5a141bb83df5ebb64e9cbad086b9d6fb076a6cc
Sivaldo Barros <104164899+sivaldobarros@users.noreply.github.com>
2022-07-23 11:18:21 -0300

    commit2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
-streamlit run your_script.py [-- script args]
+import streamlit as st
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
